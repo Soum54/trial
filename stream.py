1c949fdfa237a26d66426f06cd9e4da87168ae91
Soum54 <147833824+Soum54@users.noreply.github.com>
2024-10-09 10:07:07 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,2 +1,3 @@
 import streamlit as st
-st.balloon()
+st.markdown('<style>@keyframes move {0% {top: 100vh;} 100% {top: -50px;}} .emoji {position: absolute; left: 50%; animation: move 5s infinite;} </style><div class="emoji" style="font-size: 50px;">😊</div>', unsafe_allow_html=True)
+
